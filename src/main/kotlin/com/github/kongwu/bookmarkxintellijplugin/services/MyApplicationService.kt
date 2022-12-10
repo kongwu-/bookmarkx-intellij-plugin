5c36378de7fed66c981fdb3b2143a832cf9686f6
GitHub Action <action@github.com>
2022-12-10 08:34:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/kongwu/bookmarkxintellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kongwu/bookmarkxintellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kongwu.bookmarkxintellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kongwu.bookmarkxintellijplugin.MyBundle
 
 class MyApplicationService {
 
